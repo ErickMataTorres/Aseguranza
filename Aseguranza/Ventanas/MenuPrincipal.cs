286009c9873548552b7fff56530282a7edf5e129
Erick Mata Torres <abcdefghij110@outlook.com>
2026-08-06 04:58:00 -0700

    Eliminar inicializacion obsoleta de SQL Server

--- Aseguranza/Ventanas/MenuPrincipal.cs
+++ Aseguranza/Ventanas/MenuPrincipal.cs
@@ -1,5 +1,4 @@
 using Aseguranza.Clases;
-using Microsoft.Data.SqlClient;
 using System.Configuration;
 
 namespace Aseguranza
@@ -67,61 +66,7 @@ namespace Aseguranza
 
         private void MenuPrincipal_Load(object sender, EventArgs e)
         {
-            //try
-            //{
-            //    InicializadorBD.Inicializar();
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(
-            //        "Error inicializando la base de datos:\n" + ex.Message,
-            //        "Error crítico",
-            //        MessageBoxButtons.OK,
-            //        MessageBoxIcon.Error
-            //    );
 
-            //    Application.Exit();
-            //}
-        }
-        public static void VerificarBaseDatos()
-        {
-            string masterConn =
-                @"Server=.\SQLEXPRESS;Database=master;Trusted_Connection=True;";
-
-            string scriptPath = Path.Combine(
-                AppDomain.CurrentDomain.BaseDirectory,
-                "Database",
-                "AseguranzaBD.sql"
-            );
-
-            if (!File.Exists(scriptPath))
-                throw new Exception("No se encontró el script de la base de datos.");
-
-            string script = File.ReadAllText(scriptPath);
-
-            using (SqlConnection conn = new SqlConnection(masterConn))
-            {
-                conn.Open();
-
-                string checkDb = @"
-            IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'AseguranzaBD')
-            BEGIN
-                CREATE DATABASE AseguranzaBD;
-            END";
-
-                new SqlCommand(checkDb, conn).ExecuteNonQuery();
-            }
-
-            // Ejecutar script completo
-            string dbConn =
-                @"Server=.\SQLEXPRESS;Database=AseguranzaBD;Trusted_Connection=True;";
-
-            using (SqlConnection conn = new SqlConnection(dbConn))
-            {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand(script, conn);
-                cmd.ExecuteNonQuery();
-            }
         }
 
         private void verificadorToolStripMenuItem_Click(object sender, EventArgs e)
